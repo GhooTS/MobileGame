fe75c08f613c1daf90fffa788a425a94b4af2402
krzysztof biernat <krzysztof.biernat@edu.uekat.pl>
2020-11-15 16:46:07 +0100

    Fix Bake GL bleeding for Main Level

--- Assets/Scripts/Dynamic Collection/CoreCellDynamicCollection.cs
+++ Assets/Scripts/Dynamic Collection/CoreCellDynamicCollection.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using GTVariable;
 
 [CreateAssetMenu(menuName ="Dynamic Collection/Core Cell Collection")]
 public class CoreCellDynamicCollection : DynamicCollection<CoreCell>
